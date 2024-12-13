916508518fa2f506bc1f4711377368956e9d52e4
Ankit Suman <105882103+ankit-suman-07@users.noreply.github.com>
2024-12-13 23:25:21 +0530

    Create Demolass.java

---
+++ send/src/main/java/com/example/send/api/Demolass.java
@@ -0,0 +1,12 @@
+package com.example.send.api;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+@Data
+@Component
+public class DemoClass {
+
+
+    private String demoVal;
+}
